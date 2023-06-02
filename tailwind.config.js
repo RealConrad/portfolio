ec0c636ffdf8bb4149f0a01820769bbde303dcb9
RealConrad <wenzconrad@gmail.com>
2023-06-02 18:48:59 +0200

    added tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,26 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				light: '#fff'
+				light: '#fff',
+				darkGreen: {
+					100: '#1E2423'
+				},
+				eerieBlack: {
+					50: '#1e2221',
+					100: '#202425',
+					200: '#353E3D',
+					300: '#2A3030'
+				}
+			},
+			fontFamily: {
+				playfair: ['Playfair Display', 'serif']
 			}
+		}
+	},
+	variants: {
+		extend: {
+			animation: ['responsive', 'motion-safe', 'motion-reduce'],
+			transitionDelay: ['responsive', 'motion-safe', 'motion-reduce']
 		}
 	},
 	plugins: []
