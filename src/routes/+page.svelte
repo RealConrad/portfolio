23eaef6d062d0ef1c93c9b392301a66ecc272c94
RealConrad <wenzconrad@gmail.com>
2023-06-02 18:49:16 +0200

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,26 @@
 	import NavBar from '../components/navbar/nav-bar.svelte';
 </script>
 
-<div class="image w-screen h-screen">
-	<NavBar />
-	<div class=" h-full w-full flex justify-center items-center text-light">
-		<div class="text-8xl font-semibold">
-			<div>Conrad</div>
-			<div>Wenz</div>
+<div class="w-full h-screen z-20">
+	<section id="landing-page" class="image w-full h-full">
+		<NavBar />
+		<div class="h-full w-full flex justify-center items-center text-light text-center">
+			<div>
+				<div class="overflow-hidden">
+					<div class="text-6xl font-semibold animate-slide-in-top delay-02">Conrad Wenz</div>
+				</div>
+				<div class="overflow-hidden">
+					<div class="text-lg font-medium animate-slide-in-top delay-07">A Programmer</div>
+				</div>
+			</div>
 		</div>
-	</div>
+	</section>
+	<section class="w-full pl-60 pt-20 bg-black text-light">
+		<section id="About" class="w-full" style="height: 2000px">
+			<div>
+				<div class="">About</div>
+				<div>Some content</div>
+			</div>
+		</section>
+	</section>
 </div>
-
-<style>
-	.image {
-		background-image: url('../images/image1.jpg');
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-attachment: scroll;
-	}
-</style>
