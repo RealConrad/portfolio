e53eb98d8cc830ce053697b1abbfb3842b9234cb
RealConrad <wenzconrad@gmail.com>
2023-06-21 22:11:48 +0200

    started creating roadmap componet

---
+++ src/components/roadmap.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="mt-36 p-4 bg-darkPurple-100 rounded-md flex">
+	<div>
+		<div class="text-2xl font-bold">Intern Frontend Developer</div>
+		<div class="text-gray-400">Tesla Inc.</div>
+		<div class="pl-5 text-gray-300">
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+			galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text
+			of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+			text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+			make a type specimen book.
+		</div>
+	</div>
+</div>
