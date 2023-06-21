e578e6f6efa92ba2921156bf8ed3a104792076ca
RealConrad <wenzconrad@gmail.com>
2023-06-21 22:12:21 +0200

    added css and tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,37 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			boxShadow: {
+				glow: '0 0 50px #1D1A36'
+			},
 			colors: {
 				light: '#fff',
 				darkGreen: {
 					100: '#1E2423'
 				},
-				eerieBlack: {
-					50: '#1e2221',
-					100: '#202425',
-					200: '#353E3D',
-					300: '#2A3030'
+				darkBlue: {
+					100: '#121225'
+				},
+				primaryOrange: {
+					100: '#f94d54',
+					200: '#39FF14',
+					300: '#fe855d'
+				},
+				myGrey: {
+					// 100: '#5E6171',
+					100: '#C4BEC3',
+					200: '#38475D',
+					300: '#C4BEC3',
+					400: '#67697A'
+				},
+				richBlack: {
+					100: '#030714'
+				},
+				darkPurple: {
+					100: '#100C28'
+				},
+				space: {
+					100: '#1D1A36'
 				}
 			},
 			fontFamily: {
