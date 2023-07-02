d8092921cb7468a1d979f93165498722ec526295
RealConrad <wenzconrad@gmail.com>
2023-07-02 21:50:33 +0200

    added work experience timeline

--- src/components/roadmap.svelte
+++ src/components/roadmap.svelte
@@ -1,17 +1,61 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	let experiences: WorkExperience[] = [];
+
+	onMount(async () => {
+		const response = await fetch('data/work-experience.json');
+		const data = await response.json();
+		experiences = data.experienceData;
+	});
 </script>
 
-<div class="mt-36 p-4 bg-darkPurple-100 rounded-md flex">
-	<div>
-		<div class="text-2xl font-bold">Intern Frontend Developer</div>
-		<div class="text-gray-400">Tesla Inc.</div>
-		<div class="pl-5 text-gray-300">
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-			galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text
-			of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-			text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-			make a type specimen book.
-		</div>
+{#if experiences.length > 0}
+	<div class="relative wrap overflow-hidden p-10 h-full">
+		<div
+			class="border-2-2 absolute border-opacity-70 border-light h-full border"
+			style="left: 50%"
+		/>
+
+		{#each experiences as experience}
+			<div
+				class="{experience.isLeftSide
+					? 'flex-row-reverse'
+					: ''} mb-8 flex justify-between items-center w-full"
+			>
+				<div class="order-1 w-5/12" />
+				<div
+					class="z-20 flex items-center order-1 bg-gray-800 p-3 shadow-xl rounded-full justify-center text-center"
+				>
+					<div class="text-sm text-light font-semibold">
+						<div>
+							{experience.startMonthShort}
+							{experience.startYear}
+						</div>
+						<div>&darr;</div>
+						<div>
+							{experience.endMonthShort}
+							{experience.endYear}
+						</div>
+					</div>
+				</div>
+				<div class="order-1 bg-darkPurple-100 rounded-lg shadow-xl w-5/12 px-6 py-4">
+					<div class="font-bold text-light text-2xl">{experience.companyName}</div>
+					<div class=" text-light text-sm">
+						{experience.jobTitle}
+					</div>
+					<div class="bg-light mt-4" style="height: 1px;" />
+					<div class="text-sm tracking-wide text-myGrey-300">
+						{#each experience.workDescription as work}
+							<div class="flex gap-x-2">
+								<div class="pt-1">&#x2022</div>
+								<div class="py-1">{work}</div>
+							</div>
+						{/each}
+					</div>
+				</div>
+			</div>
+		{/each}
 	</div>
-</div>
+{:else}
+	<div>Oops! Looks like something went wrong. Try again later.</div>
+{/if}
