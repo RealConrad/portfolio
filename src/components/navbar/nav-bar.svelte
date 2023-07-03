355463074f5efea870d8e24cc4fb19ff679e2cd4
RealConrad <wenzconrad@gmail.com>
2023-07-03 22:46:21 +0200

    fixed navbar background

--- src/components/navbar/nav-bar.svelte
+++ src/components/navbar/nav-bar.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Avatar from '../avatar.svelte';
 	import NavbarLink from './navbar-link.svelte';
+
+	let sectionTop: number = 0;
+	let scrollY: number = 0;
+	let navBg: string = 'transparent';
+
+	onMount(() => {
+		const aboutSection = document.getElementById('About');
+		if (aboutSection) sectionTop = aboutSection.offsetTop - 5;
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	});
+
+	const handleScroll = () => {
+		scrollY = window.scrollY;
+		navBg =
+			scrollY >= sectionTop ? 'bg-richBlack-100 border-b-[1px] border-myGrey-200' : 'transparent';
+	};
 
 	const scrollIntoView = (id: string) => {
 		document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
 	};
 </script>
 
-<div class="fixed top-0 w-full text-light">
+<div class="fixed z-50 top-0 w-full text-light {navBg} ">
 	<div class="flex justify-between w-full items-center py-3 px-44">
 		<div
 			aria-hidden="true"
-			on:click={(e) => scrollIntoView('landing-page')}
+			on:click={() => scrollIntoView('landing-page')}
 			class="flex gap-x-5 cursor-pointer"
 		>
-			<Avatar imageUrl={'/images/profile.jpg'} />
+			<Avatar imageUrl={'/images/profile2.png'} />
 			<div class="text-lg">Conrad Wenz</div>
 		</div>
 		<div class="flex gap-x-10">
