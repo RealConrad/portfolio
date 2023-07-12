81a3843c8c4e7d1a8e5241c174af124ca6f3d1d5
RealConrad <wenzconrad@gmail.com>
2023-07-12 19:59:13 +0200

    changed header component default size

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let title: string;
-	export let size: string = 'text-5xl';
+	export let size: string = 'text-6xl';
 </script>
 
 <div class="font-bold {size} text-light">{title}</div>
